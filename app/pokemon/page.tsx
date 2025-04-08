b41ebc925a706be34fa14b0a82a7730bcb39e044
Apoorv Agarwal <aapoorv41@gmail.com>
2025-04-08 18:40:14 +0530

    Remove package-lock.json and update Pokemon page to include multiple RandomPokemonList components with varying delays

--- app/pokemon/page.tsx
+++ app/pokemon/page.tsx
@@ -5,9 +5,16 @@ export default function Page() {
   return (
     <div className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
       <p>Pokemon list</p>
-      <RandomPokemonList />
+      {/* <RandomPokemonList /> */}
+
+      <p>Pokemon list with delay</p>
       <Suspense fallback={<div>Loading...</div>}>
         <RandomPokemonList delay={2000} />
+      </Suspense>
+
+      <p>Pokemon list with delay</p>
+      <Suspense fallback={<div>Loading...</div>}>
+        <RandomPokemonList delay={3000} />
       </Suspense>
     </div>
   );
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,12 +7,7 @@ const Navbar = () => {
       <div className="container mx-auto flex justify-between items-center">
         <div className="text-white text-xl font-bold">PokeNext</div>
         <div className="space-x-4">
-          <Link
-            href="/"
-            className="text-white hover:text-blue-200 transition-colors"
-          >
-            Home
-          </Link>
+          <Link href="/">Home</Link>
           <Link href="/pokemon">Pokemon</Link>
         </div>
       </div>
